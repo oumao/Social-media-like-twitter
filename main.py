31aa4e0cd0a0777603aa8cc42586709dc7619c32
Milton <monjurhasan105142@gmail.com>
2020-05-25 17:13:04 +0600

    form success validate message show code in main.py and layout.html file

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,url_for
+from flask import Flask,render_template,url_for,flash,redirect
 from form import RegistrationForm,LoginForm
 app = Flask(__name__)
 app.config['SECRET_KEY']='c32bb949a4350a4f3ab9b179fc44f79d'
@@ -25,9 +25,13 @@ def home():
 def about():
     return render_template('about.html',title='About')
 
-@app.route('/register')
+@app.route('/register',methods=['GET','POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}!!",'success')#eikhane flash ar 2nd parameter ta hossce category ar flash bootstrap class support kore tai eikhane bootstrap ar success class diya hoise jate success ar color show kore.eikhane waring,error class o diya jaito but eikhane jehtu condition holo success ar tai success diya hoise
+    
+        return redirect(url_for('home'))
     return render_template('register.html',title='Register', form=form)
 @app.route('/login')
 def login():
