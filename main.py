f7f86648c19d92b26ff7e216da032bea337ec465
Milton <monjurhasan105142@gmail.com>
2020-05-27 00:11:27 +0600

    post table created

--- main.py
+++ main.py
@@ -19,7 +19,20 @@ class User(db.Model):
 
     def __repr__(self):
         return f"User('{self.username},{self.email},{self.image}')"
-    
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
+
+
 post_demo=[
       {
           'title':'blog',
