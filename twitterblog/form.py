e2e12a0acd6a4a4c09e47b6944e7ff5b657d5fad
Milton <monjurhasan105142@gmail.com>
2020-05-27 22:00:41 +0600

    check email and user has already exit or not code

--- twitterblog/form.py
+++ twitterblog/form.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField,BooleanField
-from wtforms.validators import DataRequired,Email,EqualTo,Length
+from wtforms.validators import DataRequired,Email,EqualTo,Length,ValidationError
+from twitterblog.models import User
 
 class RegistrationForm(FlaskForm):
     username=StringField('Username',
@@ -12,6 +13,19 @@ class RegistrationForm(FlaskForm):
     confirm_password=PasswordField('Confirm Password',
                 validators=[DataRequired(),EqualTo('password')])  
     submit=SubmitField('Sign Up')
+
+    def validate_username(self,username):
+        user=User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError("This username has already exit..So choose another username")
+    
+    
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email has already exit... Please choose a different one.')
+    
+        
 
 class LoginForm(FlaskForm):
     email=StringField('Email',
