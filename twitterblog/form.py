3619834c747d8129c0aa897af880d70ac298f211
Milton <monjurhasan105142@gmail.com>
2020-05-28 14:57:37 +0600

    login error auth 2

--- twitterblog/form.py
+++ twitterblog/form.py
@@ -1,37 +1,33 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField,BooleanField
-from wtforms.validators import DataRequired,Email,EqualTo,Length,ValidationError
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from twitterblog.models import User
 
-class RegistrationForm(FlaskForm):
-    username=StringField('Username',
-                validators=[DataRequired(), Length(min=2, max=12)])
-    email=StringField('Email',
-                validators=[DataRequired(),Email()])
-    password=PasswordField('Password',
-               validators=[DataRequired()])
-    confirm_password=PasswordField('Confirm Password',
-                validators=[DataRequired(),EqualTo('password')])  
-    submit=SubmitField('Sign Up')
 
-    def validate_username(self,username):
-        user=User.query.filter_by(username=username.data).first()
+class RegistrationForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
         if user:
-            raise ValidationError("This username has already exit..So choose another username")
-    
-    
+            raise ValidationError('That username is taken. Please choose a different one.')
+
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user:
-            raise ValidationError('That email has already exit... Please choose a different one.')
-    
-        
+            raise ValidationError('That email is taken. Please choose a different one.')
+
 
 class LoginForm(FlaskForm):
-    email=StringField('Email',
-                validators=[DataRequired(),Email()])
-    password=PasswordField('Password',
-               validators=[DataRequired()])
-    remember=BooleanField('Remember Me')
-    
-    submit=SubmitField('Login')                                 
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
--- twitterblog/routes.py
+++ twitterblog/routes.py
@@ -3,7 +3,7 @@ from twitterblog import app,db,bcrypt
 from twitterblog.form import RegistrationForm,LoginForm
 from twitterblog.models import User,Post
 from flask_login import login_user
-from werkzeug.security import generate_password_hash,check_password_hash
+
 
 
 post_demo=[
@@ -33,25 +33,20 @@ def about():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        hashed_password = generate_password_hash(form.password.data)
-        user = User(username=form.username.data,email=form.email.data,password=hashed_password)
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
         db.session.add(user)
         db.session.commit()
-        flash(" Your account has been created successfully!! You are now able to login",'success')#eikhane flash ar 2nd parameter ta hossce category ar flash bootstrap class support kore tai eikhane bootstrap ar success class diya hoise jate success ar color show kore.eikhane waring,error class o diya jaito but eikhane jehtu condition holo success ar tai success diya hoise
+        flash('Your account has been created! You are now able to log in', 'success')
+        return redirect(url_for('login'))#eikhane flash ar 2nd parameter ta hossce category ar flash bootstrap class support kore tai eikhane bootstrap ar success class diya hoise jate success ar color show kore.eikhane waring,error class o diya jaito but eikhane jehtu condition holo success ar tai success diya hoise
     
-        return redirect(url_for('login'))
     return render_template('register.html',title='Register', form=form)
 @app.route('/login',methods=['GET','POST'])
 def login():
     form = LoginForm()
-    if form.validate_on_submit():#form submit or validate ki na? eita ei function check kore true hole kaj kore
+    if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
-        print(user.password)
-        form_pass=form.password.data
-        gen=generate_password_hash(form_pass)
-        print(gen)
-        print(form_pass)
-        if (user and check_password_hash(user.password,form_pass)):
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
         
             return redirect(url_for('home'))
