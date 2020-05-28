8db0d60d84ebbec3352dadcee5ae6820a540a9f5
ouma evans <evansotis2015@gmail.com>
2020-05-28 18:14:44 +0300

    Modified the SQL connection

--- twitterblog/__init__.py
+++ twitterblog/__init__.py
@@ -4,7 +4,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY']='c32bb949a4350a4f3ab9b179fc44f79d'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///twitter.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/twitter'
 db = SQLAlchemy(app)
 login_manager=LoginManager(app)
 
