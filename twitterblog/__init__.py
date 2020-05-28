880b577fb0d11aa6077b36b2c6148c9073cc7125
Milton <monjurhasan105142@gmail.com>
2020-05-28 14:20:28 +0600

    error login auth

--- twitterblog/__init__.py
+++ twitterblog/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY']='c32bb949a4350a4f3ab9b179fc44f79d'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/twitter'
 db = SQLAlchemy(app)
 bcrypt=Bcrypt(app)
+login_manager=LoginManager(app)
 
 from twitterblog import routes
--- twitterblog/routes.py
+++ twitterblog/routes.py
@@ -2,6 +2,9 @@ from flask import render_template,url_for,flash,redirect
 from twitterblog import app,db,bcrypt
 from twitterblog.form import RegistrationForm,LoginForm
 from twitterblog.models import User,Post
+from flask_login import login_user
+from werkzeug.security import generate_password_hash,check_password_hash
+
 
 post_demo=[
       {
@@ -30,8 +33,8 @@ def about():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        hashed_password=bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user=User(username=form.username.data,email=form.email.data,password=hashed_password)
+        hashed_password = generate_password_hash(form.password.data)
+        user = User(username=form.username.data,email=form.email.data,password=hashed_password)
         db.session.add(user)
         db.session.commit()
         flash(" Your account has been created successfully!! You are now able to login",'success')#eikhane flash ar 2nd parameter ta hossce category ar flash bootstrap class support kore tai eikhane bootstrap ar success class diya hoise jate success ar color show kore.eikhane waring,error class o diya jaito but eikhane jehtu condition holo success ar tai success diya hoise
@@ -42,10 +45,17 @@ def register():
 def login():
     form = LoginForm()
     if form.validate_on_submit():#form submit or validate ki na? eita ei function check kore true hole kaj kore
-        if form.email.data=='admin@gmail.com' and form.password.data=='12345':
-            flash(f"You have been logged in!!",'success')
+        user = User.query.filter_by(email=form.email.data).first()
+        print(user.password)
+        form_pass=form.password.data
+        gen=generate_password_hash(form_pass)
+        print(gen)
+        print(form_pass)
+        if (user and check_password_hash(user.password,form_pass)):
+            login_user(user, remember=form.remember.data)
+        
             return redirect(url_for('home'))
         else:
-            flash(f"Unsuccessfull log in!! Please check username or password",'danger')
+            flash("Unsuccessfull log in!! Please check username or password",'danger')
     return render_template('login.html',title='Login', form=form)    
 
